f0eaee0c6fc3ab1277a0b293d0148cfc881b3628
Jeff Hanna <jeff.b.hanna@gmail.com>
2018-12-09 10:27:58 -0600

    Day 9 - COMPLETE

--- day_09.py
+++ day_09.py
@@ -15,31 +15,35 @@ For example, suppose there are 9 players. After the marble with value 0 is place
 
 [-] (0)
 [1]  0 (1)
-[2]  0 (2) 1 
+[2]  0 (2) 1
 [3]  0  2  1 (3)
-[4]  0 (4) 2  1  3 
-[5]  0  4  2 (5) 1  3 
-[6]  0  4  2  5  1 (6) 3 
+[4]  0 (4) 2  1  3
+[5]  0  4  2 (5) 1  3
+[6]  0  4  2  5  1 (6) 3
 [7]  0  4  2  5  1  6  3 (7)
-[8]  0 (8) 4  2  5  1  6  3  7 
-[9]  0  8  4 (9) 2  5  1  6  3  7 
-[1]  0  8  4  9  2(10) 5  1  6  3  7 
-[2]  0  8  4  9  2 10  5(11) 1  6  3  7 
-[3]  0  8  4  9  2 10  5 11  1(12) 6  3  7 
-[4]  0  8  4  9  2 10  5 11  1 12  6(13) 3  7 
-[5]  0  8  4  9  2 10  5 11  1 12  6 13  3(14) 7 
+[8]  0 (8) 4  2  5  1  6  3  7
+[9]  0  8  4 (9) 2  5  1  6  3  7
+[1]  0  8  4  9  2(10) 5  1  6  3  7
+[2]  0  8  4  9  2 10  5(11) 1  6  3  7
+[3]  0  8  4  9  2 10  5 11  1(12) 6  3  7
+[4]  0  8  4  9  2 10  5 11  1 12  6(13) 3  7
+[5]  0  8  4  9  2 10  5 11  1 12  6 13  3(14) 7
 [6]  0  8  4  9  2 10  5 11  1 12  6 13  3 14  7(15)
-[7]  0(16) 8  4  9  2 10  5 11  1 12  6 13  3 14  7 15 
-[8]  0 16  8(17) 4  9  2 10  5 11  1 12  6 13  3 14  7 15 
-[9]  0 16  8 17  4(18) 9  2 10  5 11  1 12  6 13  3 14  7 15 
-[1]  0 16  8 17  4 18  9(19) 2 10  5 11  1 12  6 13  3 14  7 15 
-[2]  0 16  8 17  4 18  9 19  2(20)10  5 11  1 12  6 13  3 14  7 15 
-[3]  0 16  8 17  4 18  9 19  2 20 10(21) 5 11  1 12  6 13  3 14  7 15 
-[4]  0 16  8 17  4 18  9 19  2 20 10 21  5(22)11  1 12  6 13  3 14  7 15 
-[5]  0 16  8 17  4 18(19) 2 20 10 21  5 22 11  1 12  6 13  3 14  7 15 
-[6]  0 16  8 17  4 18 19  2(24)20 10 21  5 22 11  1 12  6 13  3 14  7 15 
+[7]  0(16) 8  4  9  2 10  5 11  1 12  6 13  3 14  7 15
+[8]  0 16  8(17) 4  9  2 10  5 11  1 12  6 13  3 14  7 15
+[9]  0 16  8 17  4(18) 9  2 10  5 11  1 12  6 13  3 14  7 15
+[1]  0 16  8 17  4 18  9(19) 2 10  5 11  1 12  6 13  3 14  7 15
+[2]  0 16  8 17  4 18  9 19  2(20)10  5 11  1 12  6 13  3 14  7 15
+[3]  0 16  8 17  4 18  9 19  2 20 10(21) 5 11  1 12  6 13  3 14  7 15
+[4]  0 16  8 17  4 18  9 19  2 20 10 21  5(22)11  1 12  6 13  3 14  7 15
+
+# MOVE WITHOUT MODULUS 23 RULE
+[5]  0 16  8 17  4 18  9 19  2 20 10 21  5 22 11(23) 1 12  6 13  3 14  7 15
+However, if the marble that is about to be placed has a number which is a multiple of 23, something entirely different happens. First, the current player keeps the marble they would have placed, adding it to their score. In addition, the marble 7 marbles counter-clockwise from the current marble is removed from the circle and also added to the current player's score. The marble located immediately clockwise of the marble that was removed becomes the new current marble.
+
+[5]  0 16  8 17  4 18(19) 2 20 10 21  5 22 11  1 12  6 13  3 14  7 15
+[6]  0 16  8 17  4 18 19  2(24)20 10 21  5 22 11  1 12  6 13  3 14  7 15
 [7]  0 16  8 17  4 18 19  2 24 20(25)10 21  5 22 11  1 12  6 13  3 14  7 15
-The goal is to be the player with the highest score after the last marble is used up. Assuming the example above ends after the marble numbered 25, the winning score is 23+9=32 (because player 5 kept marble 23 and removed marble 9, while no other player got any points in this very short example game).
 
 Here are a few more examples:
 
@@ -49,11 +53,16 @@ Here are a few more examples:
 21 players; last marble is worth 6111 points: high score is 54718
 30 players; last marble is worth 5807 points: high score is 37305
 What is the winning Elf's score?
+
+--- Part Two ---
+Amused by the speed of your answer, the Elves are curious:
+
+What would the new winning Elf's score be if the number of the last marble were 100 times larger?
 """
 
-from collections import defaultdict, deque
+from collections import defaultdict
+from itertools import cycle
 import re
-import itertools
 
 
 def _parse( filepath: str ) -> tuple:
@@ -63,40 +72,74 @@ def _parse( filepath: str ) -> tuple:
 
 
 class Game( ):
-	def __init__( self, player_count: int, final_marble_value: int ):
+	"""
+	Instance an Elf marble game. The provided player_count and final_marble_value are used to set the state of the game and establish
+	the end-game trigger. Once the game is simulated the winning elf and their score are presented.
+	"""
+
+	def __init__( self, player_count: int, final_marble_value: int, part_2_scale : int = False ):
 		self._player_count = player_count
 		self._players = defaultdict( list )
-		self._final_marble_value = final_marble_value
+		self._final_marble_value = final_marble_value if not part_2_scale else final_marble_value * 100
 		self._current_marble_idx = 0
 		self._marble = 0
-		#self._circle = deque( [ self._marble ] )
 		self._circle = [ self._marble ]
 
 
-	def _calculate_score( self, player: int ):
-		print( "The player who put down the final marble is: ", player )
+	def _calculate_score( self ):
+		"""
+		Sums the totals of all of the elves' marbles. The winner is the elf with the highest score.
+		"""
 
-	
+		high_score = 0
+		winning_player = 0
+
+		for player, marbles in self._players.items( ):
+			score = sum( marbles )
+			if score > high_score:
+				high_score = score
+				winning_player = player
+
+		print( 'The winning elf is: {0} with a score of: {1}'.format( winning_player, high_score ) )
+
+
 	def _turn( self, player: int ):
+		"""
+		Simulates a game turn of the elf marble game. The turn rules can be found in the module docstring.
+
+		Arguments:
+			player {int} -- The current player
+		"""
+
 		self._marble += 1
-		insert_location = self._current_marble_idx + 2
-		if insert_location > len( self._circle ):
-			insert_location = len( self._circle ) - insert_location
-		
-		self._circle.insert( insert_location, self._marble )
-		self._current_marble_idx = insert_location
-		
+
+		if self._marble % 23 == 0:
+			extra_score_marble_idx = ( self._current_marble_idx - 7 ) % len( self._circle )
+			self._current_marble_idx = ( extra_score_marble_idx )
+			extra_score_marble = self._circle.pop( extra_score_marble_idx )
+			self._players[ player ].extend( [ self._marble, extra_score_marble ] )
+		else:
+			insert_location = ( self._current_marble_idx + 1 ) % len( self._circle ) + 1
+			self._circle.insert( insert_location, self._marble )
+			self._current_marble_idx = insert_location
+
 		if self._marble == self._final_marble_value:
-			self._calculate_score( player )
+			self._calculate_score( )
 
 
 	def play( self ):
-		for p in itertools.cycle( range( 1, self._player_count + 1 ) ):
+		"""
+		Public method to start the game once the initial state is established.
+		"""
+
+		for p in cycle( range( 1, self._player_count + 1 ) ):
 			self._turn( p )
 
 
 
 if __name__ == '__main__':
 	player_count, final_marble_value = _parse( r'day_09_input.txt' )
-	game = Game( player_count, final_marble_value )
-	game.play( )
+	game_1 = Game( player_count, final_marble_value )
+	game_1.play( )
+	game_2 = Game( player_count, final_marble_value, part_2_scale = True )
+	game_2.play( )
